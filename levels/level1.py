22c4cf7fc02c4c8621ff3f0eec63c2089792ae1b
Smiptatis <toreman04@gmail.com>
2022-06-02 18:46:17 +0200

    Fixed graphics part 1 TRUE ENDING COMING SOON

--- levels/level1.py
+++ levels/level1.py
@@ -17,23 +17,23 @@ def createGobbler(x, y):
 def createLevelOne(platforms, spikes, goal, walls, gobblers):
     #PLATFORM 1 250 PLATFORM LAST 4450 highest y platform from ground == 255
     platforms.add(
-    Rect(-200, 255, 300, 20, fill='grey'), 
-    Rect(-600, 160, 150, 20, fill='grey'),
-    Rect(500, 180, 300, 20, fill='grey'),
-    Rect(500, 270, 100, 20, fill='grey'),
-    Rect(700, 270, 100, 20, fill='grey'),
-    Rect(600, 290, 100, 70, fill='grey'),
-    Rect(1000, 200, 400, 20, fill='grey'),
-    Rect(1600, 150, 40, 20, fill='grey'),
-    Rect(1800, 100, 40, 20, fill='grey'),
-    Rect(2200, 50, 40, 20, fill='grey'),
+    Image('images/platform3.png', -200, 255), 
+    Image('images/platform2.png', -600, 160),
+    Image('images/platform3.png', 500, 180),
+    Image('images/platform.png', 500, 270),
+    Image('images/platform.png', 700, 270),
+    Image('images/platform.png', 600, 290),
+    Image('images/platform4.png', 1000, 200),
+    Image('images/platform5.png', 1600, 150),
+    Image('images/platform5.png', 1800, 100),
+    Image('images/platform5.png', 2200, 50),
     )
     
 
     platforms.add(
-    Rect(250, 350, 100, 20, fill='grey'), Rect(270, 340, 100, 20, fill='grey'), Rect(290, 330, 100, 20, fill='grey'), Rect(310, 320, 100, 20, fill='grey'), Rect(330, 310, 100, 20, fill='grey'),
-    Rect(350, 300, 100, 20, fill='grey'), Rect(370, 290, 100, 20, fill='grey'), Rect(390, 280, 100, 20, fill='grey'), Rect(410, 270, 100, 20, fill='grey'), Polygon(15+255, 370, 235+255, 280, 345+255, 290, 345+255, 370, fill='grey'), Polygon(600, 370, 700, 360, 600, 360, fill='grey'),
-    Rect(2800, 150, 150, 20, fill='grey'), Rect(3100, 255, 100, 20, fill='grey'), Rect(3600, 255, 100, 20, fill='grey'), Rect(3800, 200, 100, 20, fill='grey'), Rect(4200, 130, 200, 20, fill='grey'), Rect(4600, 100, 400, 20, fill='grey')
+    Image('images/platform.png', 250, 350), Image('images/platform.png', 270, 340), Image('images/platform.png', 290, 330), Image('images/platform.png', 310, 320), Image('images/platform.png', 330, 310),
+    Image('images/platform.png', 350, 300), Image('images/platform.png', 370, 290), Image('images/platform.png', 390, 280), Image('images/platform.png', 410, 270),
+    Image('images/platform2.png', 2800, 150), Image('images/platform.png', 3100, 255), Image('images/platform.png', 3600, 255), Image('images/platform.png', 3800, 200), Image('images/platform2.png', 4200, 130), Image('images/platform4.png', 4600, 100)
     )
     
     spikes.add(createSpike(800, 360, 0, 12))
@@ -47,7 +47,7 @@ def createLevelOne(platforms, spikes, goal, walls, gobblers):
 
     gobblers.add(createGobbler(2710, 360), createGobbler(3200, 360), createGobbler(3400, 360), createGobbler(3800, 360), createGobbler(3650, 255), createGobbler(4000, 360), createGobbler(-525, 160))
     spikes.add(createSpike(2710+200, 360, 0, 4))
-    goal.centerX -= 100
+    goal.centerX = 4650
 
 
 #createLevelOne()
